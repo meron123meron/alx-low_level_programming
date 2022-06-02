5203369aab107588848a2980721ccdf3f5a245e3
meron <meron@example.com>
2022-06-02 04:27:47 -0700

    i modified 4-puts.c for the third time

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
-#include <string.h>
 /**
  * main - main block
  * Return: 0
  */
 int main(void) {
 
-	char str1[] = "Programming is like building a multilingual puzzle \n";
-
-	puts(str1);
+	puts("Programming is like building a multilingual puzzle\n");
 
 return 0;
 }
