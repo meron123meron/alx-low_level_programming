4b82edcfb79c80cfb89f61baa83bc04b7734fce1
meron <meron@example.com>
2022-06-02 04:54:33 -0700

    i modified 4-puts.c for the third time i think

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <string.h>
-
+/**
+ * main - main block
+ * Return: 0
+ */
 int main(void)
 {
 
-	char str1[] = "Programming is like building a multilingual puzzle ";
+	char str1[] = "Programming is like building a multilingual puzzle";
 
 	puts(str1);
 
