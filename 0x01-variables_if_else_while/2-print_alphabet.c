9124f4acb453fd14ab5245fee70f8d9bdcbb28bd
meron <meron@example.com>
2022-06-03 06:22:05 -0700

    i modified 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,14 +4,14 @@
  * main - main block
  * Return:0
  */
-int main (void)
+int main(void)
 {
 int x;
-for (x='A'; x <='Z'; x++)
+for (x = 'A'; x <= 'Z'; x++)
 {
 int lower_x = tolower(x);
 putchar(lower_x);
+putchar('\n');
 }
-
 return (0);
 }
