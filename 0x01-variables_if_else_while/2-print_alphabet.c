dbcaf0772f9552c2f339ef76d1383a32436fdf94
meron <meron@example.com>
2022-06-03 05:00:59 -0700

    i created 2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - main block
+ * Return:0
+ */
+int main (void)
+{
+int x;
+for (x='A'; x <='Z'; x++)
+{
+int lower_x = tolower(x);
+putchar(lower_x);
+}
+
+return (0);
+}
