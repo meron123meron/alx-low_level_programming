c35efcfa2cde70da0d21ee3302189efd0a8baae0
meron <meron@example.com>
2022-06-03 06:49:50 -0700

    i created 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Return:0
+ */
+int main(void)
+
+{
+int x;
+for(x = 'a' ; x <= 'z' ; x++)
+{
+putchar(x);
+}
+for(x = 'A' ; x <= 'Z' ; x++)
+{
+putchar(x);
+}
+return(0);
+}
