b7126d4afc7bcdafadc8b927d88580577ebc3f30
meron <meron@example.com>
2022-06-03 03:19:02 -0700

    i erased more spaces in1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,17 +12,17 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 lastDigit = n % 10;
 if (lastDigit > 5)
-	{
+{
 printf("Last digit of %i is ", n);
 printf("%i and is greater than 5\n", lastDigit);
-	}
+}
 else
-	{
+{
 if (lastDigit == 0)
-       {
+{
 printf("Last digit of %i is ", n);
 printf("%i and is 0\n", lastDigit);
-       }
+}
 else
 {
 printf("Last digit of %i is ", n);
@@ -30,4 +30,4 @@ printf("%i and is less than 6 and not 0\n", lastDigit);
 }
 }
 return (0);
-  }
+}
