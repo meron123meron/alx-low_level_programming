3aa9154c64b80acf80fe2732fbd55a5906edaa20
meron <meron@example.com>
2022-06-05 01:52:28 -0700

    i created 8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * print numbers 0-9 and a-f
+ * Return:0
+ */
+int main(void)
+{
+int h;
+for (h = 48;h < 58;h++)
+{
+putchar(h);
+}
+for (h = 97;h < 103;h++)
+{
+putchar(h);
+}
+
+putchar('\n');
+return (0);
+}
