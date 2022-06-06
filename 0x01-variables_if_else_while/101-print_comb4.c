fbd5906ad60abae1b42bd89ab7af5d8f2b79244e
meron <meron@example.com>
2022-06-06 02:22:41 -0700

    i modified 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main
- * print 012, 013, 
+ * main - print 012, 013, 
+ *
  * Return:0
  */
 int main(void)
@@ -9,16 +9,16 @@ int main(void)
 int a, b, c;
 for (a = 48 ; a <= 55 ; a++)
 {
-for (b = 49 ; b <=56 ; b++)
+for (b = 49 ; b <= 56 ; b++)
 {
-for (c = 50 ; c <=57 ; c++)
+for (c = 50 ; c <= 57 ; c++)
 {
-if((a < b) || (a < c) || (b < c))
+if ((a < b) || (a < c) || (b < c))
 {
 putchar(a);
 putchar(b);
 putchar(c);
-if((a < 55) || (b < 56) || (c < 57))
+if ((a < 55) || (b < 56) || (c < 57))
 {
 putchar(',');
 putchar(' ');
