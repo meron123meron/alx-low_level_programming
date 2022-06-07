031e3d323900202f8407966ba04c71eaf292a73c
meron <meron@example.com>
2022-06-07 09:23:42 -0700

    i created 1-alphabet.c anf modified main.h

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - main block
+ * print alphabet
+ * Return:0
+ */
+
+void print_alphabet(void)
+{
+int x;
+
+for (x = 'a'; x <= 'z'; x++)
+{
+_putchar(x);
+}
+_putchar('\n');
+}
