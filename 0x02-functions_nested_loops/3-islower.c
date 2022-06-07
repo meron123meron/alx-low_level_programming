61e1396b9f190dffd154494cc33f23084aa3cd1e
meron <meron@example.com>
2022-06-07 13:10:15 -0700

    i created 3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _islower - checks if c is lowercase or not
+ * Return:void
+ */
+int _islower(int c)
+{
+
+if (_islower(c))
+{
+_putchar('1');
+}
+else
+{
+_putchar('0');
+}
+_putchar('\n');
+return (0);
+}
