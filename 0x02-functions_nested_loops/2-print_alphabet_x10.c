b8cbc3f4693490bf519f781cba55578e2933e0d7
meron <meron@example.com>
2022-06-07 11:05:16 -0700

    i created 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main -main block
+ * prints alphabet 10 times
+ * Return:0
+ */
+void print_alphabet_x10(void)
+{
+int y, z;
+for (y = 1; y <= 10; y++)
+{
+for(z = 'a'; z <= 'z'; z++)
+{
+_putchar(z);
+
+}
+_putchar('\n');
+}
+}
