d91f107f65f1e3feebe434a49a252d62a269a5b2
meron <meron@example.com>
2022-06-07 11:10:17 -0700

    i modified 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,12 +5,13 @@
  * prints alphabet 10 times
  * Return:0
  */
+//prints alphabets 10 times
 void print_alphabet_x10(void)
 {
 int y, z;
 for (y = 1; y <= 10; y++)
 {
-for(z = 'a'; z <= 'z'; z++)
+for (z = 'a'; z <= 'z'; z++)
 {
 _putchar(z);
 
