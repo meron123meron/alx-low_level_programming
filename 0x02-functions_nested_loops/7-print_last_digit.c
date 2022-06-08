28c1d8b1c9eea80bb704a15389732a33b969c424
meron <meron@example.com>
2022-06-08 10:50:51 -0700

    i created 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,10 +2,10 @@
 #include  "main.h"
 /**
  * print_last_digit - prints the last digit of a number
+ * @a:is character used
  * Return:the value of the lat digit
  */
-int print_last_digit(int)
+int print_last_digit(int a)
 {
-int n;
-return (n % 10);
+return (a % 10);
 }
