3c1c1261132209242d74b3fcc49e1084579aff2a
meron <meron@example.com>
2022-06-08 02:47:38 -0700

    i modified 4-isalpha.c again

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -7,22 +7,6 @@
  */
 int _isalpha(int c)
 {
-if (c >= 97 && c <= 122) 
-{
-return (1);
-}
-else
-
-{
-return (0);
-}
-if (c >= 65 && c <= 90)
-{
-return (1);
-}
-else
-{
-return (0);
-}
-
+return (c >= 97 && c <= 122);
+return (c >= 65 && c <= 90);
 }
