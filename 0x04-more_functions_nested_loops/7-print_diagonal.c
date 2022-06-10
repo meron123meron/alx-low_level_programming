a33678ba1a85541c4997859f27d4c7b70d3d3777
meron <meron@example.com>
2022-06-10 13:15:42 -0700

    i created 7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_diagonal - draws a diagonal line
+ * @n:is number of times \ is printed
+ * Return:0
+ */
+void print_diagonal(int n)
+{
+int a;
+for (a = 0; a < n; a++)
+{
+_putchar(92);
+_putchar('\n');
+_putchar(32);
+}
+}
