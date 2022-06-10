29925c2b57051c68779ea66d99d878b646a1a9c0
meron <meron@example.com>
2022-06-10 13:27:55 -0700

    i modified 7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,11 +6,16 @@
  */
 void print_diagonal(int n)
 {
-int a;
+int a, b;
 for (a = 0; a < n; a++)
 {
 _putchar(92);
 _putchar('\n');
+
+for (b = 0; b < a; b++)
+{
 _putchar(32);
 }
+}
+_putchar('\n');
 }
