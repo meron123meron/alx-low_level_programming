353cbf076b0a7a744082a6563b9432101f7d996c
meron <meron@example.com>
2022-06-10 05:42:02 -0700

    i created 6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_line - draws a straight line
+ * Return:0
+ */
+void print_line(int n)
+{
+int a = 0;
+if (a < n)
+{
+_putchar('_');
+a++;
+}
+
+_putchar('\n');
+
+}
