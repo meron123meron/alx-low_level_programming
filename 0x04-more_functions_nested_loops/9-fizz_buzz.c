03706004b9470a4a0535bedd4651826e2e8ace84
meron <meron@example.com>
2022-06-10 14:45:46 -0700

    fixing a problem in 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,29 +7,26 @@ int main(void)
 {
 int a = 1;
 printf("%d", a);
-putchar(32);
+
 for (a = 2; a <= 100; a++)
 {
 
 if ((a % 3 == 0) && (a % 5 == 0))
 {
 printf("FizzBuzz");
-putchar(32);
 }
 else if (a % 5 == 0)
 {
 printf("Buzz");
-putchar(32);
 }
 else if (a % 3 == 0)
 {
 printf("Fizz");
-putchar(32);
 }
 else
 {
 printf("%d", a);
-putchar(32);
+
 }
 }
 
