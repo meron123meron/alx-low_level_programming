f69ebf67cead85cb33c54a086e8eca2b7502412d
meron <meron@example.com>
2022-06-10 04:10:43 -0700

    i created 4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_most_numbers - print numbers from 0 to 9
+ * Return:0
+ */
+void print_most_numbers(void)
+{
+
+int c;
+while (c <= 9)
+{
+_putchar(c);
+c++;
+}
+
+_putchar('\n');
+
+}
