a37501cb06999a6f5bbda9d4f9be45f8fb17ed69
meron <meron@example.com>
2022-06-10 05:02:24 -0700

    i created 5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * more_numbers - prints 10 times the numbers from 0 to 14
+ * Return:0
+ */
+void more_numbers(void)
+{
+int a, b;
+for (a = 0; a <= 9; a++)
+{
+for (b = 0; b <= 14; b++)
+{
+_putchar(b);
+}
+_putchar('\n');
+}
+}
