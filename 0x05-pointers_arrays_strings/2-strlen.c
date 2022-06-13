d190b568113fa70b4d9a0a6d8ba106ed7bde50d3
meron <meron@example.com>
2022-06-13 06:58:42 -0700

    i created 2-strlen.c and it was 'kebad' .

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of a string
+ * @s: is name of the pointer
+ *
+ * Return:0
+ */
+int _strlen(char *s)
+{
+int z;
+for (z = 0; s[z] != '\0'; z++)
+{
+
+}
+return (z);
+}
