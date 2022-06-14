82f46602b0a82c00f4c5a96daf5f4e9b19e971e8
meron <meron@example.com>
2022-06-14 05:12:11 -0700

    i created 7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * puts_half - prints the last half of a strig
+ * @str: character used
+ *
+ * Return:0
+ */
+void puts_half(char *str)
+{
+int i;
+while (str[i] != str[i/2])
+{
+i++;
+}
+for (; i != '\0'; i++)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
+}
