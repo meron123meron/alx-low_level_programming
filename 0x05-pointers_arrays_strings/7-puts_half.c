593472af898694fc1624615199ca66daef388a92
meron <meron@example.com>
2022-06-14 13:44:13 -0700

    i modified 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,14 +7,22 @@
  */
 void puts_half(char *str)
 {
-int i;
+int i = 0;
+int n = i - 1;
 while (str[i] != str[i / 2])
 {
 i++;
-}
 for (; i != '\0'; i++)
-{
 _putchar(str[i]);
+}
+while (i % 2 != 0)
+{
+while (str[i] != str[n / 2])
+{
+i++;
+for (; i != '\0'; i++)
+_putchar(str[i]);
+}
 }
 _putchar('\n');
 }
