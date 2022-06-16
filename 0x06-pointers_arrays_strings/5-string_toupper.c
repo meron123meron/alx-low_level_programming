4eadb0d18327a9faccdfbf04c9dc673b671d569e
meron <meron@example.com>
2022-06-16 05:05:03 -0700

    i created 5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * string_toupper - changes lowercase letters to uppercase
+ *
+ * Return:some value
+ */
+char *string_toupper(char *n)
+{
+int i = 0;
+while (n[i] != '\0')
+{
+if (n[i] >= 'a' && n[i] <= 'z')
+{
+n[i] = n[i] - 32;
+}
+i++;
+}
+return (n);
+}
