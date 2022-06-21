d79f055d0842755fcb150cc44e3df893116c1573
meron <meron@example.com>
2022-06-21 13:48:57 -0700

    i created 8-print_diagsums but it doesnt run it needs modification

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_diagsums - prints the sum of two diagonals of a square matrix
+ * @a: pointer used
+ * @size: integer
+ * Return:0
+ */
+void print_diagsums(int *a, int size)
+{
+int x, z, d1 = 0, d2 = 0;
+for (x = 0; x < size; x++)
+{
+for (z = 0; z < size; z++)
+{
+if ( x == z)
+{
+d1 = d1 + a[x][z];// it shows error
+}
+else
+{
+d2 = d2 + a[x + 1][z - 1];// it shows error
+}
+}
+printf("%d%d",d1,d2);
+}
+}
