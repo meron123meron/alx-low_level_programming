05f1e4d75b9c1df9bdf46199a0f273a77c39d0d8
meron <meron@example.com>
2022-06-22 15:17:48 -0700

    i created 6-is_prime_number.c

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * is_prime_number - returns 1 if the number is prime
+ * @n: integer
+ * Return:1 or 0
+ */
+int is_prime_number(int n)
+{
+if (n % 2 != 0)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
