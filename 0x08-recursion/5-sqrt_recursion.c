510185434f589c69735f368fc45630d15908ab33
meron <meron@example.com>
2022-06-23 07:57:55 -0700

    i created 5-sqrt_recursion.c but it needs modification

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: integer
+ * Return: some number
+ */
+int _sqrt_recursion(int n)
+{
+int z = 0;
+if (n == 0 || n == 1)
+{
+return (n);
+}
+else if(z * z == n)
+{
+return (z);
+}
+else if (n < 0)
+{
+return (-1);
+}
+else if (z * z < n || z < n)
+{
+z++;
+_sqrt_recursion(n);
+}
+return (-1);
+}
