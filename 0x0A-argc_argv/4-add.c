8279aee5cba886040083731080ee6f6c1f246afe
meron <meron@example.com>
2022-06-24 16:03:26 -0700

    it really needs modification

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -9,21 +9,18 @@
  */
 int main(int argc, char **argv)
 {
-int i, sum = 0;
-if (argc == 1)
-{
-printf("0\n");
-break;
-}
-
-if (argc >= 3)
+int i = 0, sum = 0;
+if (argc > 2)
 {
 for (i = 0; i < argc; i++)
 {
 sum = sum + atoi(argv[i]);
 printf("%d\n", sum);
-break;
 }
+}
+if (i == 1)
+{
+printf("0\n");
 }
 else
 {
