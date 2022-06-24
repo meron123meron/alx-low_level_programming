c1b5f088b9ce17636533551a022e4281082cfd94
meron <meron@example.com>
2022-06-24 08:54:04 -0700

    i removed the top and bottom #

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,9 +1,3 @@
-#ifndef MAIN_H
-
-#define MAIN_H
-
-
-
 #include <stdio.h>
 
 
@@ -13,6 +7,3 @@ int main(void);
 int largest_number(int a, int b, int c);
 int convert_day(int month, int day);
 void print_remaining_days(int month, int day, int year);
-
-
-#endif /* MAIN_H */
