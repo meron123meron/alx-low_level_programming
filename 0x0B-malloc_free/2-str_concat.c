a525f5cd5fc60a65fd905db1b9ef4df225395d83
meron <meron@example.com>
2022-06-28 12:43:56 -0700

    i modified 2-concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,22 +9,28 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-int size;
+int j = 0, i = 0;
 char *p;
-strcat(s1, s2);
-size = strlen(s1);
-p = (char *)malloc(sizeof(char) * size + 1);
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
+for (; s1[i] != '\0'; i++)
+for (; s2[j] != '\0'; j++)
+
+p = (char *)malloc(sizeof(char) * (i + j + 1));
 if (p == NULL)
 {
 return (NULL);
 }
 
-while (*s1)
+for (i = 0; s1[i] != '\0'; i++)
 {
-*p = *s1;
-p++;
-s1++;
+p[i] = s1[i];
 }
-
+for (j = 0; s2[j] != '\0'; i++, j++)
+{
+p[i] = s2[j];
+}
 return (p);
 }
