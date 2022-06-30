8c85ff6e78bcbae9009d856929e4fa56771fc8e1
meron <meron@example.com>
2022-06-30 03:52:43 -0700

    i created 0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: integer used
+ * Return: Nothing
+ */
+void *malloc_checked(unsigned int b)
+{
+int *p;
+p = malloc(b);
+if (p != NULL)
+{
+return (p);
+free(p);
+}
+else
+exit (98);
+}
