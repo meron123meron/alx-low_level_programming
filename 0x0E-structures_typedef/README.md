4235d28ea7689336e52d11eb0bb6f1957e437836
meron <meron@example.com>
2022-07-04 02:23:12 -0700

    i created dog.h

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+This is a directory which includes codes using structures and typedef.
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - is a struct with 3 members
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ * Description:
+ */
+typedef struct dog
+{
+char *name;
+float age;
+char *owner;
+} dog;
+#endif
