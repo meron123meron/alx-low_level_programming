46f713bcf7cd72c91c85f4f1e81651d259ded8d5
meron <meron@example.com>
2022-07-04 06:47:09 -0700

    i created 1-init_dog.c but it requires modification

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * init_dog - is the function consisting the struct
+ * struct dog - is a struct with 4 members
+ * @d: pointer member 1
+ * @name: member 2
+ * @age: member 3
+ * @owner: member 4
+ * Description:
+ */
+
+struct dog
+{
+struct dog *d;
+char *name;
+float age;
+char *owner;
+};
