e456da36da1aef5ceb522dd252f17649eb4dfbd1
meron <meron@example.com>
2022-07-05 00:37:20 -0700

    fourth modification

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,13 +13,9 @@ if (d)
 printf("Name: %s\n", d->name);
 printf("Age: %f\n", d->age);
 printf("Owner: %s\n", d->owner);
-}
 if (d->name == NULL)
-{
 printf("Name: (nil) \n");
-}
 if (d->owner == NULL)
-{
 printf("Owner: (nil) \n");
 }
 if (d == NULL)
