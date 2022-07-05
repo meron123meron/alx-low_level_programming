93a6ff2f1582dcb5eaafd99f840186ce08a20134
meron <meron@example.com>
2022-07-05 01:44:32 -0700

    i created 4-new_dog.c

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * new_dog - is a function that receives a pointer
+ * @name: function argument
+ * @age: function argument
+ * @owner: function argument
+ * Return: pointer
+ *
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+dog_t *p = malloc(sizeof(dog_t));
+if (p == NULL)
+{
+return (NULL);
+}
+
+p->name = name;
+p->age = age;
+p->owner = owner;
+return (p);
+}
