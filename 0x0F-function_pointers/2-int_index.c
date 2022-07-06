43b5de50dbee08c8e66571610f9ba3d43eb5ba36
meron <meron@example.com>
2022-07-06 05:37:23 -0700

    i modified task 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -15,15 +15,17 @@ if (size <= 0)
 {
 return (-1);
 }
-
+if (cmp != 0 && array != 0)
+{
 for (i = 0; i < size; i++)
 {
 cmp(array[i]);
-}
-if (*(*cmp) != 0)
+if (cmp(array[i]) == 0)
 {
 return (i);
 }
+}
 
+}
 return (-1);
 }
