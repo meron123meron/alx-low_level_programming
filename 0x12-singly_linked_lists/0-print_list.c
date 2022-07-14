70ca7e683396e70249665859f1762605849d3ab3
meron <meron@example.com>
2022-07-14 03:17:56 -0700

    second try

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,19 +9,21 @@
  * Return: the number of nodes
  */
 
-typedef struct list_s
-{
-char *str;
-unsigned int len;
-struct list_s *next;
-} list_t;
 size_t print_list(const list_t *h)
 {
-int c = 0;
+size_t c = 0;
 while (h != NULL)
 {
+if (h->str == NULL)
+{
+printf("[0] (nil)\n");
+}
+else
+{
+printf("%s%u", h->str, h->len);
 c++;
 h = h->next;
+}
 }
 return (c);
 }
