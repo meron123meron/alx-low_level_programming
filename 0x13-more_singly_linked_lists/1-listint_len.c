0443c483267a60c9b42ecb09a62e92a7e7b0e856
meron <meron@example.com>
2022-07-18 02:09:54 -0700

    i created 1-listint_len.c

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * listint_len - a function that returns the number of elements
+ * @h:pointer
+ */
+size_t listint_len(const listint_t *h)
+{
+size_t c = 0;
+while (h != NULL)
+{
+c++;
+h = h->next;
+}
+return (c);
+}
