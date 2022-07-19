b560e40ddc47bb503c366057cb4a1efe16bac93e
meron <meron@example.com>
2022-07-19 12:55:05 -0700

    second try

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -11,7 +11,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 listint_t *p = *head;
 listint_t *g = *head;
-if (*head == NULL)
+if (*head == NULL || head == NULL)
 {
 return (-1);
 }
