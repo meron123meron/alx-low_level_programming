7d9727faa53d49bfce27c46b4f090af8508d65fc
meron <meron@example.com>
2022-07-21 02:53:39 -0700

    second try

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,20 +8,18 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-int i = 0, len, j, sum = 0;
+unsigned int i = 0, len, sum = 0;
+int j;
 len = strlen(b);
 if (b == NULL)
 return (0);
-else if (b[i] != 0 || b[i] != 1)
+else if (b[i] != '0' && b[i] != '1')
 return (0);
-else
-{
 for (j = (len - 1); j >= 0; j--, i++)
 {
-if (b[i] == 1)
+if (b[i] == '1')
 {
 sum = sum + power(2, j);
-}
 }
 }
 return (sum);
@@ -34,7 +32,7 @@ return (sum);
  */
 unsigned int power(unsigned int a, unsigned int b)
 {
-int i, p =1;
+unsigned int i, p =1;
 for (i = b; i > 0; i--)
 {
 p = p * a;
