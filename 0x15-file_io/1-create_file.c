8a9270b89ebbba0ead8d023489c2f260aaa7af48
meron <meron@example.com>
2022-07-25 15:14:47 -0700

    first try

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ * create_file - a function that creates a file.
+ * @filename: pointer
+ * @text_content: a string
+ * Return: 1 or -1
+ */
+int create_file(const char *filename, char *text_content)
+{
+int fd, fd_write, len;
+len = strlen(text_content);
+if (filename == NULL)
+return (-1);
+fd = open(filename, O_CREAT | O_RDWR, 0600);
+if (fd == -1)
+return (-1);
+fd_write = write(fd, text_content, len);
+if (fd_write == -1)
+return (-1);
+close(fd);
+return (1);
+}
