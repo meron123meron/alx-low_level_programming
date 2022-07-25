bb4589d43d9a52eab4fcbba81209775304470c0e
meron <meron@example.com>
2022-07-25 13:52:03 -0700

    first try but time consuming

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include "main.h"
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: pointer
+ * @letters: the number of letters it should read and print
+ * Return: the actual number of letters it could read and print or 0
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+int file, write_file;
+char *buf;
+buf = malloc(sizeof(char) * letters);
+if (buf == NULL)
+return (0);
+if (filename != NULL)
+{
+file = open("task0", O_RDONLY);
+if (file == -1)
+return (0);
+read(file, buf, letters);
+close(file);
+file = open("task0", O_CREAT | O_WRONLY, 0600);
+if (file == -1)
+return (0);
+write_file = write(file, buf, letters);
+close(file);
+return (write_file);
+}
+else
+return (0);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,1 @@
+This is a directory where operations on a single file descriptor(read, write) are exercised.
