77269c749c3fb5605d7249f19a2e802fb3554f24
meron <meron@example.com>
2022-09-01 06:49:15 -0700

    betty error resolved

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -10,7 +10,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 dlistint_t *p, *t;
 p = malloc(sizeof(dlistint_t));
 if (p == NULL)
-return NULL;
+return (NULL);
 
 p->n = n;
 p->next = NULL;
