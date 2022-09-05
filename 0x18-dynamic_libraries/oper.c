0da4c71bb60594cfae603f38b0845607f19418ed
meron <meron@example.com>
2022-09-05 14:14:37 -0700

    first trial

---
+++ 0x18-dynamic_libraries/oper.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * _add - adds two numbers
+ * @a: number 1
+ * @b: number 2
+ * _sub - subtracts two numbers
+ * _mul - multiplicates two numbers
+ * _div - divides two numbers
+ * _mod - gives the remainder after division
+ *
+ * Return: integer value
+ */
+int add(int a, int b)
+{
+return ((a) + (b));
+}
+int sub(int a, int b)
+{
+return ((a) -(b));
+}
+int mul(int a, int b)
+{
+return ((a) * (b));
+}
+int div(int a, int b)
+{
+return (a / b);
+}
+int mod(int a, int b)
+{
+return (a % b);
+}
