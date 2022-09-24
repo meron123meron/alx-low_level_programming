4d2ebe0ccd8f4ac617c8ace5826f103d3858b9f5
meron <meron@example.com>
2022-09-24 08:43:14 -0700

    task 3

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,44 @@
+#include "hash_tables.h"
+/**
+ * hash_table_set - a function that adds an element to the hash table.
+ * @ht: is the hash table
+ * @key: is the key(can not be an empty string)
+ * @value: is the value(can be an empty string)
+ * Return: 1 or 0
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+unsigned long int index, size;
+
+hash_node_t *new_node;
+
+size = ht->size;
+
+index = key_index((const unsigned char *)key, size);
+
+if (ht  == NULL || key == NULL)
+
+return (0);
+
+
+if (ht->array[index] != NULL && strcmp(ht->array[index]->key, key) == 0)
+
+{
+
+ht->array[index]->value = strdup(value);
+
+return (1);
+
+}
+
+new_node = malloc(sizeof(hash_node_t));
+if (new_node == NULL)
+
+	return (0);
+
+new_node->key = strdup(key);
+new_node->value = strdup(value);
+new_node->next = ht->array[index];
+ht->array[index] = new_node;
+return (1);
+}
