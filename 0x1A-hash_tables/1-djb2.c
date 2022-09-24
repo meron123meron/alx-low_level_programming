8f89d2753c961e020b6989f04c6852a5ea61828a
meron <meron@example.com>
2022-09-24 07:15:33 -0700

    i forgot to add description for the function

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,3 +1,10 @@
+#include  "hash_tables.h"
+/**
+ * hash_djb2 - a hash function implementing the djb2 algorithm.
+ * @str: string used to generate hash value
+ * Return: hash value
+ */
+
 unsigned long int hash_djb2(const unsigned char *str)
 
 {
@@ -10,7 +17,7 @@ hash = 5381;
 while ((c = *str++))
 {
 
-hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+hash = ((hash << 5) + hash) +c; /* hash * 33 + c */
 }
 
 return (hash);
