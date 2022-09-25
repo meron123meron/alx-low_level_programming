f7d4b64fb9a7b8717179c75dec70226d8ac637cd
meron <meron@example.com>
2022-09-25 01:47:11 -0700

    third try

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -6,8 +6,8 @@
  */
 void hash_table_print(const hash_table_t *ht)
 {
-hash_node_t *c;
-unsigned long int i = 0;
+hash_node_t *c; /*c is a node */
+unsigned long int i = 0, count = 0;
 if (ht != NULL)
 {
 printf("{");
@@ -16,8 +16,10 @@ for (i = 0; i < ht->size; i++)
 c = ht->array[i];
 while (c != NULL)
 {
-printf("'%s' : '%s'", c->key, c->value);
+if (count != 0)
 printf(", ");
+count = 1;
+printf("'%s' : '%s'", c->key, c->value);
 c = c->next;
 }
 }
