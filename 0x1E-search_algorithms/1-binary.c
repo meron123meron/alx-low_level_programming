c5b163cf326c07e9f7003eb80e27d7e22bcbe44f
meron <meron@example.com>
2023-01-04 19:42:53 +0000

    requirement fail error

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -7,7 +7,6 @@
  * @array: pointer to the first element of the array
  * @size: the number of elements in array
  * @value: the value to search for
- * You can assume that array will be sorted in ascending order
  * Return: first index where value is located or -1
  */
 int binary_search(int *array, size_t size, int value)
